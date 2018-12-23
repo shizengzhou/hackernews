54b64cc82e90a26d75973568d086ee74de582c25
szz <shizengzhou@outlook.com>
2018-12-24 07:53:28 +0800

    Add unit tests with Enzyme

--- src/components/Table/test.js
+++ src/components/Table/test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Table from '.';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Table', () => {
   const props = {
@@ -24,5 +28,13 @@ describe('Table', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
+  })
 
 });
